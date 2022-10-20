683ba9f945d7bbb5a0281e4020f45b95c90cd767
mrunal <mrunalbhoir100@gmail.com>
2022-10-20 12:41:42 +0530

    Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HelloWorld';
+  imgUrl="HelloWorld\logo.jpg";
+  url="https:/www.google.com";
+  userName:string="";
+  nameError:string="";
+
+  ngOnInit(): void{
+    this.title="hello world 1"
+  }
+
+onClick($event: any){
+  console.log("save button is clicked",$event)
+  window.open(this.url,"_blank");
+}
+
+onInput($event:any){
+  console.log("change event occured!",$event.data);
+  const nameRegex=RegExp('[A-Z]{1}[a-z]{2}');
+  if(nameRegex.test(this.userName)){
+    this.nameError="";
+    return;
+  }
+  this.nameError="name is incorrect";
+}
 }
